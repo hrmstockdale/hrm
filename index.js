8ddba2fbc487f751134277db6d0a652e5763f638
Henry <hrmstockdale@gmail.com>
2019-06-18 23:11:48 +0100

    farage barrage project added

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const myPicture = document.getElementsByClassName('myPicture')[0];
-const phoneIcon = document.getElementsByClassName('icon_grid-phone')[0];
-console.log('<-------- check Elements for my contact number');
+const myPicture = document.getElementsByClassName("myPicture")[0];
+const phoneIcon = document.getElementsByClassName("icon_grid-phone")[0];
+console.log("<-------- check Elements for my contact number");
 
-
-( () => {
-    let i = 0;
-    const images = ['assets/henry_no_background002_small.png','assets/henry_no_background003_small.png'];
-    const toggle = () => {
-        myPicture.src = images[i];
-        i = (i + 1) % images.length
-    }
-    setInterval(toggle, 2000)
+(() => {
+  let i = 0;
+  const images = [
+    "assets/henry_no_background002_small.png",
+    "assets/henry_no_background003_small.png"
+  ];
+  const toggle = () => {
+    myPicture.src = images[i];
+    i = (i + 1) % images.length;
+  };
+  setInterval(toggle, 2000);
 })();
 
-phoneIcon.addEventListener('click', () => {
-    alert('grab my number from the devTools ツ')
-})
+phoneIcon.addEventListener("click", () => {
+  alert("grab my number from the devTools ツ");
+});
